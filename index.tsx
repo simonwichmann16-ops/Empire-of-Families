d1180377b28a853e7ca6223f9e263a4cd4c875a9
simonwichmann16-ops <simonwichmann16@gmail.com>
2025-08-15 11:33:38 +0200

    commit

--- index.tsx
+++ index.tsx
@@ -1,7 +1,7 @@
 
 import React from 'react';
 import ReactDOM from 'react-dom/client';
-import App from '.src/App.tsx';
+import App from './src/App.tsx';
 
 const rootElement = document.getElementById('root');
 if (!rootElement) {
